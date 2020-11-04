9295d67433c20cc5e098176c1cdec5573b95ac38
Nguyen Khoi Tuan <n10418377@qut.edu.au>
2020-11-04 11:53:45 +1000

    Hoang modification

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print('Hello Alo')
+    print('Hello Alo 123132')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
